a8a339d3559f0eda111eb7147f90288824425438
sanjidajannat <sanjidakadir525@gmail.com>
2024-03-30 13:52:36 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide',{
+        perPage: 5,
+        rewind : true,
+    } );
+    splide.mount();
+  } );
+
+
+
+  const cartbtn = document. getElementById
+  ('add-cart')
+  function addcartfunction(){
+    cartbtn .innerHTML ="added to cart"
+  }
